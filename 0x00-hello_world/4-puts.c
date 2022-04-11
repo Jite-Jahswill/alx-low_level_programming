7623f73c1631ae328ca1e1c5409c1998a1b6b4cc
Jite-Jahswill <jitejahswill@gmail.com>
2022-04-11 20:29:25 +0200

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the phrase "programming is like building a multilingual puzzle"
- * Return: is zero
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+}	
