6d578231bed314bf067408a8b1427a10336f99da
Jite-Jahswill <jitejahswill@gmail.com>
2022-04-21 16:53:10 +0200

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,26 @@
-#include "main.h"
+#include ""[Dmain.h"
 
 /**
- * _strlen -length of a string
- * @s: A pointer to an int that will be changed/updated
+ *_strlen -length of a string
  *
- * Return: void that means our answer is correct
+ *
+ *[A[A@s: A pointer to an init that will be changed/updated
+
+[C*Return: void that means our answer is correct
  */
 
-int _strlen(char*s)
+int  _strlen(char *s)[B[B[B
 {
-int u;
+int u:
+u = 0;[A
+
+[A[A[C[C[C[C[C[B[B[A[B[B[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[A[C[C[C[C[C[C[C[C[B[A
 
 u = 0;
-while (s[u] != '\0')
+while ()[Ds[][B[A[B[Du]  !='\0')
 {
 u++;
 }
-return (u);
-}
+return
+[A[C[C[C[C[C[C[C[D[ (u);
+}[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[[C[C[[C[C[C[C![C[C !='\0')[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
