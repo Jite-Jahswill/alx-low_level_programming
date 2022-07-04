80e855ad225e6f9ee35ec4e489cff728d0bb9ce0
Jite-Jahswill <jitejahswill@gmail.com>
2022-07-04 15:33:01 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: prints all single digit numbers of base 10
- * starting from 0, followed by a new line.
- * Return: 0
- */
+  * main - Prints a serie of numbers with commas
+  *
+  * Return: Always (Success);
+  */
 int main(void)
 {
-int c = 0;
+	int c;
 
-while (c < 10)
-{
-putchar(48 +c);
-if (c != 9)
-{
-putchar(',');
-putchar(' ');
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
-c++;
-}
-putchar('\n');
-return (0);
-}
-
-
-
-
-
-
