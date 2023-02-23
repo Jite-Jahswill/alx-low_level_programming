44467835b6a835d8a399243994272d1129a4142e
Jite-Jahswill <jitejahswill@gmail.com>
2023-02-23 10:51:57 -0800

    task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,17 @@
-#include "holberton.h"
+#include "main.h"
+
+/**
+* _isupper - returns 1 if uppercase alphabet and 0 otherwise
+* @c: takes in a character
+* Return: 0 for uppercase, 1 for lowercase
+**/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}#include "holberton.h"
 
 /**
   * _isupper - Check if a letter is upper
