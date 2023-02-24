bb17290658167aa53ff0239e7793c71805342ff3
Jite-Jahswill <jitejahswill@gmail.com>
2023-02-24 04:59:50 -0800

    task all

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-* _isupper - returns 1 if uppercase alphabet and 0 otherwise
+* _isdigit - Returns 1 if c is a digit
 * @c: takes in a character
 * Return: 0 for uppercase, 1 for lowercase
 **/
-int _isupper(int c)
+int _isdigit(int c)
 {
-if (c >= 'A' && c <= 'Z')
+if (c >= '0' && c <= '9')
 return (1);
 else
 return (0);
