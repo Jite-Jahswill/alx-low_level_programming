85dc4be8434b6fe7c593ef4bc329301449166efd
Jite-Jahswill <jitejahswill@gmail.com>
2023-03-16 04:28:26 -0700

    task 2

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,17 @@
-#include <stdlib.h>
 #include "main.h"
-/*
- * malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
- * Return: a pointer to the allocated memory
- */
+#include <stdlib.h>
+/**
+* malloc_checked - a function that allocates memory using malloc
+* @b: unsigned integer
+* Return: Returns a pointer to the allocated memory
+**/
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	unsigned int *n;
 
-	ptr = malloc(b);
+	n = malloc(b);
 
-	if (ptr == NULL)
+	if (n == NULL)
 		exit(98);
-
-	return (ptr);
+	return (n);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,45 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/*
- * string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- * Return: pointer to the resulting string
- */
+
+/**
+* string_nconcat - concatentates two strings using the newly allocated memory
+* @s1: first string
+* @s2: second string
+* @n: unsigned int
+* Return: a pointer to 2 string concat..
+**/
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
+	int l, i;
+	unsigned int j;
+	char *p;
 
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	l = 0;
+	i = 0;
+	j = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-
-	while (s2 && s2[len2])
-		len2++;
-
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-
-	else
-
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (!s)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[l])
+		l += 1;
+	p = malloc(l + n + 1);
+	if (p == NULL)
 		return (NULL);
-
-	while (i < len1)
+	while (s1[i])
 	{
-		s[i] = s1[i];
-
-		i++;
+		p[i] = s1[i];
+		i += 1;
 	}
 
-	while (n < len2 && i < (len1 + n))
-		s[i++] = s2[j++];
-
-	while (n >= len2 && i < (len1 + len2))
-		s[i++] = s2[j++];
-
-	s[i] = '\0';
-
-	return (s);
+	while (j < n)
+	{
+		p[i + j] = s2[j];
+		j += 1;
+	}
+	p[i + j] = '\0';
+	return (p);
 }
